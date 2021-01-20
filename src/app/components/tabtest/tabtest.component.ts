2591ad6912f5a7dd667ec47339fe172cf2dc7ba8
Shashi <shashikant.kewale@synechron.com>
2021-01-20 13:32:13 +0530

    Final

--- src/app/components/tabtest/tabtest.component.ts
+++ src/app/components/tabtest/tabtest.component.ts
@@ -11,7 +11,9 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class TabtestComponent implements OnInit {
   regForm: FormGroup;
   person: any;
-  elist: Array<any>
+  elist: Array<any>;
+  inde: number;
+  updateFlag: boolean
 
   constructor(private frmBuilder: FormBuilder) {
     this.regForm = this.frmBuilder.group({
@@ -21,6 +23,8 @@ export class TabtestComponent implements OnInit {
       esalary:""
     });
     this.elist = []
+    this.inde= 0
+    this.updateFlag=false
 
    }
 
@@ -35,6 +39,42 @@ export class TabtestComponent implements OnInit {
     console.log(this.regForm.value);
     this.elist.push(this.regForm.value);
     console.log(this.elist);
+
+    this.regForm.reset()
+  }
+
+  eupdate(p: any, e: Event){
+    e.preventDefault();
+    console.log(p)
+    this.updateFlag = true;
+    this.regForm = this.frmBuilder.group({
+      eid: p.eid,
+      ename: p.ename,
+      edesignation:p.edesignation,
+      esalary:p.esalary
+    });
+
+  }
+
+  edelete(p: any, e: Event){
+    e.preventDefault();    
+    this.inde = this.elist.findIndex((e)=>p.eid===e.eid)
+    console.log(this.inde)
+    this.elist.splice(this.inde,1)
+    console.log(this.elist)
+    this.regForm.reset()
+  }
+
+  update_submit(){
+
+    this.inde = this.elist.findIndex((e)=>this.regForm.value.eid===e.eid)
+
+    this.elist[this.inde].eid = this.regForm.value.eid;
+    this.elist[this.inde].ename = this.regForm.value.ename;
+    this.elist[this.inde].edesignation = this.regForm.value.edesignation;
+    this.elist[this.inde].esalary = this.regForm.value.esalary;
+    
+    //this.regForm.reset()
   }
 
 }
